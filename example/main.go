e8ff064ba3d3d608b33be69ff0287a6b1dadc5c4
mralexandermalvarez <mralexandermalvarez@gmail.com>
2020-09-15 17:12:24 -0700

    modified example file

--- example/main.go
+++ example/main.go
@@ -10,11 +10,10 @@ import (
 const tokenURL = "https://us.battle.net/oauth/token"
 
 func main() {
-	myArray := [100] int
 	client := bnet.BnetClient{Client: bnet.CreateClient(os.Getenv("CID"), os.Getenv("CSID"), tokenURL)}
 	tophundred, topprofiles := ulocal.GetTopHundredArenaSpec(&client, 29, "3v3", "paladin", "holy")
 	for i, entry := range tophundred.Entries {
-		fmt.Println(entry.Character.Name, entry.Rating, topprofiles[i].)
+		fmt.Println(entry.Character.Name, entry.Rating, topprofiles[i])
 	}
 
 }
